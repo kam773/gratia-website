01f4b41a571808c069da8c31fcf10bf290961cd8
Kamil <developer.kamil90@gmail.com>
2018-10-12 16:14:43 +0200

    add mobile nav

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,10 @@
 /* Shadow on scroll navbar */ 
 function scrollMenu() {
     var menu = document.querySelector('.navigation');
-    var menuSection = document.querySelector('.navigation-menu');
     if (window.scrollY > 0) {
         menu.classList.add('shadow');
-        menuSection.classList.add('shadow');
     } else {
-        menu.classList.remove('shadow');
-        menuSection.classList.remove('shadow');
+        menu.classList.remove('shadow');   
     }
 };
 
@@ -24,3 +21,16 @@ function scrollMenuReservation() {
 };
 
 window.addEventListener('scroll', scrollMenuReservation);
+
+
+/* Scroll to */ 
+$('.btn-arrow').click(function () {
+    $('html, body').animate({scrollTop: $('.perfection').offset().top}, 500); 
+});
+
+ /* Toggle menu */
+$(document).ready(function() {
+    $('.toggle').click(function() {
+       $('ul').toggleClass('active');
+    });
+});
